44a8b77e4cdf1826d8630ad47a08396d19ba3fe7
lhcardoso <c.luizhenrique@bmail.com>
2020-08-19 20:48:56 -0300

    worker

--- NewTask/NewTask.cs
+++ NewTask/NewTask.cs
@@ -1,40 +1,33 @@
-﻿using RabbitMQ.Client;
-using System;
+﻿using System;
+using RabbitMQ.Client;
 using System.Text;
 
-namespace NewTask
+class NewTask
 {
-    public class NewTask
+    public static void Main(string[] args)
     {
-        public static void Main()
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            {
-                channel.QueueDeclare(
-                    queue: "hello",
-                    durable: false,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null
-                    );
+            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-                var message = GetMessage(args);
-                var body = Encoding.UTF8.GetBytes(message);
-                var properties = channel.CreateBasicProperties();
-                properties.Persistent = true;
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(
-                    exchange: "",
-                    routingKey: "task_queue",
-                    basicProperties: properties,
-                    body: body
-                    );
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
 
-                Console.WriteLine($" [x] Sent {message}");
-            }
-            Console.WriteLine(" Press [enter] to exit");
+            channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
         }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
     }
 }
